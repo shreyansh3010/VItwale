c51c14558e4fe0b698f7a7e21921568ae389d93b
Chirag Arora <chirag.arora2015@vit.ac.in>
2017-04-13 18:13:54 +0530

    Splash Screen Only.

---
+++ app/src/main/java/com/vitwale/vitwale/SplashScreen/Splash.java
@@ -0,0 +1,54 @@
+package com.vitwale.vitwale.SplashScreen;
+
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import com.vitwale.vitwale.MainActivity;
+import com.vitwale.vitwale.R;
+
+public class Splash extends AppCompatActivity {
+
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    // Splash screen pause time
+                    while (waited < 5000) {
+                        sleep(100);
+                        waited += 100;
+                    }
+                    Intent intent = new Intent(Splash.this,
+                            MainActivity.class);                            //NavigationDrawer
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    Splash.this.finish();
+                } catch (InterruptedException e) {
+                    // do nothing
+                } finally {
+                    Splash.this.finish();
+                }
+
+            }
+        };
+        thread.start();
+    }
+}
+
