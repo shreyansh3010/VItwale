67386e22096782313112e1f044b977544fdb2688
shreyansh <shreyansh.2015@vit.ac.in>
2017-04-24 00:46:20 +0530

    change the home

--- app/src/main/java/com/vitwale/vitwale/Home/HomeFragment.java
+++ app/src/main/java/com/vitwale/vitwale/Home/HomeFragment.java
@@ -1,12 +1,19 @@
 package com.vitwale.vitwale.Home;
 
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.daimajia.slider.library.Animations.DescriptionAnimation;
@@ -14,6 +21,17 @@ import com.daimajia.slider.library.SliderLayout;
 import com.daimajia.slider.library.SliderTypes.BaseSliderView;
 import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.daimajia.slider.library.Tricks.ViewPagerEx;
+import com.firebase.client.Firebase;
+import com.firebase.client.annotations.NotNull;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 import com.vitwale.vitwale.R;
 
 import java.util.HashMap;
@@ -24,9 +42,11 @@ import java.util.HashMap;
  */
 public class HomeFragment extends Fragment implements BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener {
 
-    View view;
     private SliderLayout mDemoSlider;
-
+    private RecyclerView mBlogList;
+    private DatabaseReference mDatabase;
+    private ProgressDialog mprogress;
+    View view;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -39,55 +59,126 @@ public class HomeFragment extends Fragment implements BaseSliderView.OnSliderCli
         // Inflate the layout for this fragment
         view =  inflater.inflate(R.layout.fragment_home, container, false);
         mDemoSlider = (SliderLayout) view.findViewById(R.id.slider);
-        HashMap<String,String> url_maps = new HashMap<String, String>();
 
-        url_maps.put("1. Eride", "http://vitwale.com/images/carousel/eride111.jpg");
-        url_maps.put("2. Riviera", "http://vitwale.com/images/carousel/carousel_2.jpg");
-        url_maps.put("3. The Electronics Club", "http://vitwale.com/images/carousel/TEC_Banner1.png");
-        url_maps.put("4. Entrepreneurship Cell", "http://vitwale.com/images/carousel/ECELL_Recruitment_CR.jpg");
+        mDatabase = FirebaseDatabase.getInstance().getReferenceFromUrl("https://vitwale-e0508.firebaseio.com");
+        mDatabase.keepSynced(true);
+
+        mDatabase.child("home_slider").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                    String img1 = dataSnapshot.child("img1").getValue(String.class);
+                    String img2 = dataSnapshot.child("img2").getValue(String.class);
+                    String img3 = dataSnapshot.child("img3").getValue(String.class);
+                    String img4 = dataSnapshot.child("img4").getValue(String.class);
+                HashMap<String,String> url_maps = new HashMap<String, String>();
+
+                url_maps.put("1. Eride", img1);
+                url_maps.put("2. Riviera", img2);
+                url_maps.put("3. The Electronics Club", img3);
+                url_maps.put("4. Entrepreneurship Cell", img4);
+                for(String name : url_maps.keySet()){
+                    TextSliderView textSliderView = new TextSliderView(getActivity());
+                    // initialize a SliderLayout
+                    textSliderView
+                            .description(name)
+                            .image(url_maps.get(name))
+                            .setScaleType(BaseSliderView.ScaleType.Fit);
+
+                    //add your extra information
+                    textSliderView.bundle(new Bundle());
+                    textSliderView.getBundle()
+                            .putString("extra",name);
+
+                    mDemoSlider.addSlider(textSliderView);
+
+                }
+
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
 
-       /* HashMap<String,Integer> file_maps = new HashMap<String, Integer>();
-        file_maps.put("Hannibal",R.drawable.a);
-        file_maps.put("Big Bang Theory",R.drawable.a);
-        file_maps.put("House of Cards",R.drawable.a);
-        file_maps.put("Game of Thrones", R.drawable.a);
-*/
-        for(String name : url_maps.keySet()){
-            TextSliderView textSliderView = new TextSliderView(getActivity());
-            // initialize a SliderLayout
-            textSliderView
-                    .description(name)
-                    .image(url_maps.get(name))
-                    .setScaleType(BaseSliderView.ScaleType.Fit)
-                    .setOnSliderClickListener(this);
 
-            //add your extra information
-            textSliderView.bundle(new Bundle());
-            textSliderView.getBundle()
-                    .putString("extra",name);
+        mBlogList = (RecyclerView) view.findViewById(R.id.blog_list);
+        mBlogList.setHasFixedSize(true);
+        mBlogList.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mBlogList.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
 
-            mDemoSlider.addSlider(textSliderView);
-        }
+
+
         mDemoSlider.setPresetTransformer(SliderLayout.Transformer.Stack);
         mDemoSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
         mDemoSlider.setCustomAnimation(new DescriptionAnimation());
         mDemoSlider.setDuration(3000);
         mDemoSlider.addOnPageChangeListener(this);
-        /*ListView l = (ListView)findViewById(R.id.transformers);
-        l.setAdapter(new TransformerAdapter(this));
-        l.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                mDemoSlider.setPresetTransformer(((TextView) view).getText().toString());
-                Toast.makeText(MainActivity.this, ((TextView) view).getText().toString(), Toast.LENGTH_SHORT).show();
-            }
-        });*/
-
 
 
         return view;
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseRecyclerAdapter<Blog1, BlogViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Blog1, BlogViewHolder>(
+
+                Blog1.class,
+                R.layout.blog_row,
+                BlogViewHolder.class,
+                mDatabase.child("Blog")
+        ) {
+            @Override
+            protected void populateViewHolder(BlogViewHolder viewHolder, Blog1 model, int position) {
+                viewHolder.setTitle(model.getTitle());
+                viewHolder.setDesc(model.getDesc());
+                viewHolder.setImage(getActivity(),model.getImage());
+                //mprogress.dismiss();
+            }
+        };
+        mBlogList.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    public static class BlogViewHolder extends RecyclerView.ViewHolder {
+
+
+
+        View mView;
+        public BlogViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        public void setTitle(String title){
+
+            TextView post_title = (TextView) mView.findViewById(R.id.post_title);
+            post_title.setText(title);
+        }
+
+        public void setDesc(String desc){
+            TextView post_desc = (TextView) mView.findViewById(R.id.post_text);
+            post_desc.setText(desc);
+        }
+
+        public void setImage(Context ctx, String image){
+            final ProgressBar mprogressBar;
+            mprogressBar = (ProgressBar) mView.findViewById(R.id.progressBar5);
+            ImageView post_image = (ImageView) mView.findViewById(R.id.post_image);
+            Picasso.with(ctx).load(image).into(post_image, new Callback() {
+                @Override
+                public void onSuccess() {
+                    mprogressBar.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onError() {
+
+                }
+            });
+        }
+    }
+
+
 
     @Override
     public void onStop() {
